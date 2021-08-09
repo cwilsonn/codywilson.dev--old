08baa43d432f9bf5153eb71256204a27b0409f63
Cody Wilson <cody@tangent-digital.com>
2021-08-08 17:09:06 -0700

    Fix: Updated logo to be more easily read.

--- src/components/BrandLogo.vue
+++ src/components/BrandLogo.vue
@@ -4,19 +4,7 @@
   >
     <span id="logo-text" class="m-auto">
       <g-link to="/" aria-label="C(2*U)" class="flex items-center">
-        <span>C(2</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="inline w-3 h-3 md:w-5 md:h-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd" />
-        </svg>
-        <span>U)</span>
+        <span>CW</span>
       </g-link>
     </span>
   </span>
